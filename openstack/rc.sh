ff6e64ad605376e8eb1f74e3c9aa7dce46d23978
tellrv <vyasrohitvyas@gmail.com>
2019-07-10 16:24:13 +0530

    modiefied rc

--- openstack/rc.sh
+++ openstack/rc.sh
@@ -10,7 +10,7 @@
 # OpenStack API is version 3. For example, your cloud provider may implement
 # Image API v1.1, Block Storage API v2, and Compute API v2.0. OS_AUTH_URL is
 # only for the Identity API served through keystone.
-export OS_AUTH_URL=https://rhos-d.infra.prod.upshift.rdu2.test.com:13000/v3
+export OS_AUTH_URL=https://test.test.com:13000/v3
 # With the addition of Keystone we have standardized on the term **project**
 # as the entity that owns the resources.
 export OS_PROJECT_ID=d2d61ed7f19ce7b0eab
